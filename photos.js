cf6060383bbb30ea73a52eb54d64560a342086f4
Tate Wilhelm <tatewilhelm@yahoo.com>
2021-02-15 19:07:30 -0500

    Add pics, github placeholder link

--- photos.js
+++ photos.js
@@ -1,7 +1,10 @@
 
 const pics = [
+    {filename: "LaneWinter.JPG", caption: "Lane Stadium in the snow"},
+    {filename: "TruckOnBeach.JPG", caption: "The Gladiator on the beach during golden hour"},
     {filename: "DJI_0099.jpg", caption: "Jeeps on the beach in the Outer Banks"},
-    {filename: "DJI_0101.jpg", caption: "Checking out the terrain"}
+    {filename: "DJI_0101.jpg", caption: "Checking out the terrain"},
+    {filename: "DJI_0104.jpg", caption: "In the in the drone zone at the beach"}
 ];
 
 window.addEventListener("load", () => loadPics(), false);
