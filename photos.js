86ecac6fef598f4c2062cd0475b072ff56f41328
Tate Wilhelm <tatewilhelm@yahoo.com>
2021-02-16 19:10:43 -0500

    Add videos, add more caption info to pics

--- photos.js
+++ photos.js
@@ -1,10 +1,10 @@
 
 const pics = [
-    {filename: "LaneWinter.JPG", caption: "Lane Stadium in the snow"},
-    {filename: "TruckOnBeach.JPG", caption: "The Gladiator on the beach during golden hour"},
-    {filename: "DJI_0099.jpg", caption: "Jeeps on the beach in the Outer Banks"},
-    {filename: "DJI_0101.jpg", caption: "Checking out the terrain"},
-    {filename: "DJI_0104.jpg", caption: "In the in the drone zone at the beach"}
+    {filename: "LaneWinter.JPG", caption: "Lane Stadium in the snow", camera: "iPhone 11", date: "January 2021"},
+    {filename: "TruckOnBeach.JPG", caption: "The Gladiator on the beach during golden hour", camera: "iPhone 11", date: "December 2020"},
+    {filename: "DJI_0099.jpg", caption: "Jeeps on the beach in the Outer Banks", camera: "DJI Mavic 2", date: "December 2020"},
+    {filename: "DJI_0101.jpg", caption: "Checking out the terrain", camera: "DJI Mavic 2", date: "December 2020"},
+    {filename: "DJI_0104.jpg", caption: "In the in the drone zone at the beach", camera: "DJI Mavic 2", date: "December 2020"}
 ];
 
 window.addEventListener("load", () => loadPics(), false);
@@ -12,13 +12,33 @@ window.addEventListener("load", () => loadPics(), false);
 function loadPics() {
     const picsEl = document.getElementById("pics");
     pics.forEach(pic => {
+        const div = document.createElement("div");
+        div.setAttribute("class", "image");
+        picsEl.appendChild(div);
+        
         const img = document.createElement("img");
-        const caption = document.createElement("p");
-        caption.setAttribute("class", "caption")
         img.setAttribute("src", `pics/${pic.filename}`);
+        div.appendChild(img);
+
+        const captionDiv = document.createElement("div");
+        captionDiv.setAttribute("class", "captionDiv");
+        div.appendChild(captionDiv);
+
+        const caption = document.createElement("p");
+        caption.setAttribute("class", "caption");
         caption.innerHTML = pic.caption;
-        picsEl.appendChild(img);
-        picsEl.appendChild(caption);
+        captionDiv.appendChild(caption);
+
+        const date = document.createElement("p");
+        date.setAttribute("class", "date");
+        date.innerHTML = `Date: ${pic.date}`;
+        captionDiv.appendChild(date);
+
+        const camera = document.createElement("p");
+        camera.setAttribute("class", "camera");
+        camera.innerHTML = `Camera: ${pic.camera}`;
+        captionDiv.appendChild(camera);
+        
     });
 }
 
